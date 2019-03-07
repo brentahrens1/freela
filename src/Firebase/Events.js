66238be34521d1abf494e6ae780baf2c1270b7c8
Brent Ahrens <cidida@gmail.com>
2019-03-07 06:52:59 -0800

    Adds nature page

--- src/Firebase/Events.js
+++ src/Firebase/Events.js
@@ -10,3 +10,19 @@ export const doGetMusicEvents = () =>
     eventsRef
         .where('category', '==', 'Music')
         .get()
+
+
+export const doGetNatureEvents = () =>
+    eventsRef
+        .where('category', '==', 'Nature')
+        .get()
+
+export const doGetExerciseEvents = () =>
+    eventsRef
+        .where('category', '==', 'Exercise')
+        .get()
+
+export const doGetArtEvents = () =>
+    eventsRef
+        .where('category', '==', 'Art')
+        .get()
--- src/Nature/Nature.js
+++ src/Nature/Nature.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Nature.css'
+import EventCard from '../EventCard/EventCard'
+import { doGetNatureEvents } from '../Firebase/Events'
 
-const Nature = () => {
-    return(
+class Nature extends Component {
+    state = {
+        events: []
+    }
+    componentDidMount() {
+        doGetNatureEvents()
+            .then(snapShot => this.setState({events: snapShot.docs.map(d => d.data())}))
+    }
+
+    render () {return(
         <div className="nature-container">
-            <img className="nature-image" src="https://www.kuhl.com/borninthemountains/wp-content/uploads/2018/05/Hidden-Trail.jpeg" alt="nature"></img>
-            <div className="nature-banner">Nature</div>
+            <div className="nature-banner-flex">
+                <img className="nature-image" src="https://www.kuhl.com/borninthemountains/wp-content/uploads/2018/05/Hidden-Trail.jpeg" alt="nature"/>
+                <div className="desc-flex">
+                    <h1 className="nature-header">Nature</h1>
+                    <p className="desc-nature">No matter your genre preference, LA's got an active music scene for your taste -- and probably 
+                        has a totally free ongoing night that'll please you, aurally. Here are LA's best free ongoing 
+                        music nights,  we'd love to hear anything else we missed in the comments -- Please feel free to register 
+                        and post any upcoming live shows coming up.</p>
+                    <h1 className="nature-header">Live Feed</h1>
+                </div>
+            </div>
+            <hr/>
+            <h1 className="upcoming-header">All that nature has to offer</h1>
+            <hr/>
+            <div className="nature-flex">
+                {
+                    this.state.events.map(e => 
+                        <EventCard event={e}/>
+                    )
+                }
+            </div>
         </div>
-    )
+    )}
 }
 
-export default Nature 
+export default Nature
+
+
+
